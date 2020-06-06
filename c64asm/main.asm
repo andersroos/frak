e7931f9bbf8d056a512b04f4450605d3a5860da7
Anders Roos <anders.roos@gmail.com>
2020-06-06 23:18:02 +0200

    Write something to the screen.

--- c64asm/main.asm
+++ c64asm/main.asm
@@ -16,6 +16,8 @@
         
         BasicUpstart2(start) // Creates a basic sys line that can starts the program.
 
-start:  inc $d020
-        jmp start
-
+start:  jsr $e544 // clear screen
+        jsr $e566 // move home
+        lda #$41
+        jsr $e716 // write char to screen
+wait:   jmp wait
