93b1cdfe0b821ce516de52d7d3590751238af48f
Anders Roos <anders.roos@gmail.com>
2020-06-07 11:34:40 +0200

    Testing to set and ref base mem.

--- c64asm/main.asm
+++ c64asm/main.asm
@@ -16,8 +16,45 @@
         
         BasicUpstart2(start) // Creates a basic sys line that can starts the program.
 
-start:  jsr $e544 // clear screen
-        jsr $e566 // move home
-        lda #$41
-        jsr $e716 // write char to screen
+        *=$1000 "Code"
+
+start:  // clear screen
+        jsr $e544 
+
+        // multi color mode
+        lda $d011 
+        ora #$20
+        sta $d011
+
+        // multi color mode
+        lda $d016 
+        ora #$10
+        sta $d016
+
+        // gfx mem starts at $2000
+        lda $d018
+        ora #$08
+        sta $d018
+
+        // gfx mem at $fb $2000
+        lda #$00
+        sta $fb  // lo byte
+        lda #$20
+        sta $fc  // hi byte
+
+
+        
+        // lda #$ff
+        // sta $2000
+        // lda #$00
+        // sta $2001
+        
+        
+        lda #$ff
+        ldx #$00
+        sta ($fb,x)
+        lda #$00
+        ldx #$01
+        sta ($fb,x)
+        
 wait:   jmp wait
