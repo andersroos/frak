8c4bdb3225aeeee94dd89bbd41c7930a9b2d6302
Anders Roos <anders.roos@gmail.com>
2020-06-08 12:56:54 +0200

    Removed some junk.

--- simple-js/frac.js
+++ simple-js/frac.js
@@ -1,18 +1,20 @@
-var xc = 0;
-var yc = 0;
 
-function drawPixel() {
-    for (let yc = 0; yc < 100; ++yc) {
-        for (let xc = 0; xc < 100; ++xc) {
+function draw() {
+    const canvas = document.getElementById("frac");
+    const context = canvas.getContext("2d");
+    context.fillStyle = "rgb(0,0,0)";
+    context.fillRect(0, 0, 200, 200);
+
+    const max_iteration = 32;
     
-            var canvas = document.getElementById("frac");
-            var context = canvas.getContext("2d");
-            var x0 = (-50 + xc)/25;
-            var y0 = (-50 + yc)/25;
-            var x = 0;
-            var y = 0;
-            var iteration = 0;
-            var max_iteration = 32;
+    const xd = 4/100;
+    const yd = 4/100;
+    
+    for (let yc = 0, y0 = -2; yc < 100; ++yc, y0 += yd) {
+        for (let xc = 0, x0 = -2; xc < 100; ++xc, x0 += xd) {
+            let iteration = 0;
+            let x = 0;
+            let y = 0;
             while ((x*x + y*y <= 4) && (iteration < max_iteration)) {
                 var xtemp = (x*x) - (y*y) + x0;
                 y = (2*x*y) + y0;
